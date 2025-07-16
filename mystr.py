8c4b2c9f429b5b8036d7730991b5b9c9f8399290
Omar Ahmd <om9556or@gmail.com>
2025-07-17 01:53:49 +0300

    upload

---
+++ mystr.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from transformers import BlipProcessor , BlipForConditionalGeneration
+from PIL import Image
+#-----------------------------------------------------------------------------
+st.header("ERC Image Captioning and Documentation")
+st.subheader("Please wait up to three minutes while the model is loading. In the meantime, feel free to watch this video")
+st.video("vid.mp4",autoplay=True)
+#----------------------------------------------------------------------------    
+@st.cache_resource
+def load_blip_model():
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+    return processor, model
+
+processor, model = load_blip_model()
+
+file = st.file_uploader("Upload image to model",["png","jpg","jpeg","bmh"])
+if file is not None :
+    st.image(file)
+    image = Image.open(file).convert('RGB')
+    inputs = processor(images=image , return_tensors='pt')
+    out = model.generate(**inputs)
+    caption = processor.decode(out[0],skip_special_tokens = True)
+    st.subheader("caption for image is : ")
+    st.write(caption)
+
+    st.subheader("Try another image and tell us your opinion please , and thanks")
